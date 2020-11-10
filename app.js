bb326091313777a71bf430c8ee5e5d2c89b358a6
pranav <pranav7020@gmail.com>
2020-11-10 08:22:51 +0530

    smooth swipe

--- app.js
+++ app.js
@@ -3,19 +3,39 @@ const container = document.querySelector('.dummy');
 const close = document.querySelector('.close');
 const swipeUp = document.querySelector('.swipeup-container');
 
-let touchCount = 0;
+let gestureX = [];
+let gestureY = [];
+let tolerance = 100;
 
-swipeUp.addEventListener('touchmove', touchmove, false);
+swipeUp.addEventListener('wheel', () => {
+    swipeup();
+});
 
-function touchmove(e) {
-    touchCount += e.touches.length;
-    if (touchCount > 3)
-        swipeup()
-}
+swipeUp.addEventListener('touchstart', (e) => {
+    for (i = 0; i < e.touches.length; i++) {
+        gestureX.push(e.touches[i].clientX);
+        gestureY.push(e.touches[i].clientY);
+    }
+});
+swipeUp.addEventListener('touchmove', (e) => {
+    for (var i = 0; i < e.touches.length; i++) {
+        gestureX.push(e.touches[i].clientX);
+        gestureY.push(e.touches[i].clientY);
+    }
+});
+swipeUp.addEventListener('touchend', (e) => {
+    xTravel = gestureX[gestureX.length - 1] - gestureX[0];
+    yTravel = gestureY[gestureY.length - 1] - gestureY[0];
+    if (xTravel < tolerance && xTravel > -tolerance && yTravel < -tolerance) {
+        swipeup();
+    }
+    gestureX = [];
+    gestureY = [];
+    xTravel = yTravel = '';
+});
 
 const swipeup = () => {
     popup.classList.toggle('active');
     container.classList.toggle('blur')
     close.classList.toggle('rotate');
-    touchCount = 0;
-}
+};
